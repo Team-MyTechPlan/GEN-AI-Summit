5dee50fdc1e2e5b79c5097ffeeee5346e4692532
¨LuisEUM¨ <luiseum95@gmail.com>
2024-08-17 13:01:35 +0200

    setting basic files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    unoptimized: false,
+    remotePatterns: [
+      { hostname: "localhost" },
+      { hostname: "images.unsplash.com" },
+      { hostname: "lh3.googleusercontent.com" },
+      { hostname: "drive.google.com" },
+    ],
+  },
+};
 export default nextConfig;
