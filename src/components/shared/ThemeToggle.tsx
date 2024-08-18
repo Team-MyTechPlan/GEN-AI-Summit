bd5b68ac181fff6d3456ea99879455f21cd54e18
¨LuisEUM¨ <luiseum95@gmail.com>
2024-08-18 15:36:19 +0200

    Dark Mode and Language done git push

--- src/components/shared/ThemeToggle.tsx
+++ src/components/shared/ThemeToggle.tsx
@@ -28,13 +28,13 @@ export default function ThemeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          {t("Light")}
+          {t("light")}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          {t("dark")}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          {t("system")}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
